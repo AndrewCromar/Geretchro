2ccb161dfdf3f90d2065482c0d4ea2c96b24d553
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-05 23:09:43 -0800

    inputs being detected

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,22 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 #include <TFT_eSPI.h>
 
-TFT_eSPI tft = TFT_eSPI();
-
-const int TARGET_FPS = 60;
-const int FRAME_DELAY = 1000 / TARGET_FPS;
-unsigned long delta_time = 0;
-
-int x_pos = 0;
-
-void frame();
-
-void setup()
-{
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-
-  delta_time = millis();
+void setup() {
+  pinMode(15, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
 }
 
-void loop()
-{
-  unsigned long current_time = millis();
-
-  if (current_time - delta_time >= FRAME_DELAY)
-  {
-    delta_time = current_time;
-    frame();
-  }
-}
-
-void frame()
-{
-  tft.fillRect(x_pos, 100, 20, 20, TFT_BLACK);
-
-  x_pos = (x_pos + 2) % 320;
-
-  tft.fillRect(x_pos, 100, 20, 20, TFT_GREEN);
+void loop() {
+  Serial.print("UP: ");
+  Serial.print(digitalRead(15));
+  Serial.print(", RIGHT: ");
+  Serial.print(digitalRead(14));
+  Serial.print(", DOWN: ");
+  Serial.print(digitalRead(13));
+  Serial.print(", LEFT: ");
+  Serial.println(digitalRead(12));
 }
