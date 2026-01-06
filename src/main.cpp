a4a11331d98bb107f896e51b3032590274422d2e
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-05 23:15:59 -0800

    clean up input code

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,48 @@
 #include <SD.h>
 #include <TFT_eSPI.h>
 
-void setup() {
-  pinMode(15, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+
+int button_up_pin = 15;
+int button_right_pin = 14;
+int button_down_pin = 13;
+int button_left_pin = 12;
+int button_up_input;
+int button_right_input;
+int button_down_input;
+int button_left_input;
+
+void setup_inputs();
+void get_inputs();
+void print_inputs();
+
+void setup()
+{
+  setup_inputs();
 }
 
-void loop() {
-  Serial.print("UP: ");
-  Serial.print(digitalRead(15));
-  Serial.print(", RIGHT: ");
-  Serial.print(digitalRead(14));
-  Serial.print(", DOWN: ");
-  Serial.print(digitalRead(13));
-  Serial.print(", LEFT: ");
-  Serial.println(digitalRead(12));
+void loop()
+{
+  get_inputs();
+  print_inputs();
+}
+
+void setup_inputs()
+{
+  pinMode(button_up_pin, INPUT_PULLUP);
+  pinMode(button_right_pin, INPUT_PULLUP);
+  pinMode(button_down_pin, INPUT_PULLUP);
+  pinMode(button_left_pin, INPUT_PULLUP);
+}
+
+void get_inputs()
+{
+  button_up_input = digitalRead(button_up_pin);
+  button_right_input = digitalRead(button_right_pin);
+  button_down_input = digitalRead(button_down_pin);
+  button_left_input = digitalRead(button_left_pin);
+}
+
+void print_inputs()
+{
+  Serial.println(String("UP: ") + button_up_input + " RIGHT: " + button_right_input + " DOWN: " + button_down_input + " LEFT: " + button_left_input);
 }
