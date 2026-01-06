c037bf98ae9dc236a55b5194285c043ae323e31b
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-05 20:57:06 -0800

    commit before i try cpp

--- src/code.py
+++ src/code.py
@@ -1,10 +1,19 @@
+import graphics
 import time
 
-import graphics
-import interpreter
-
-gui = graphics.GraphicsSystem()
-gui.testdraw()
+gui = graphics.GraphicsEngine()
+pos_x = 0
 
 while True:
-    time.sleep(0.1)
+    gui.clear()
+    
+    pos_x = (pos_x + 2) % 160
+    
+    gui.draw_square(pos_x, 30, 20, 0x1cfc0c)
+    gui.draw_square(100, 10, 5, 0xFF0000)
+    gui.draw_square(10, 80, 10, 0x0000FF)
+    
+    for i in range(30):
+        gui.draw_pixel(i + 20, 110, 0xFFFFFF)
+    
+    gui.render()
--- src/graphics.py
+++ src/graphics.py
@@ -4,24 +4,74 @@ import displayio
 from fourwire import FourWire
 from adafruit_st7789 import ST7789
 
-class GraphicsSystem:
+class GraphicsEngine:
     def __init__(self):
         displayio.release_displays()
+        
         spi = busio.SPI(clock=board.GP18, MOSI=board.GP19)
-        display_bus = FourWire(spi, command=board.GP20, chip_select=board.GP17, reset=board.GP21)
-        
-        self.dimensions = (320, 240)
-        self.rotation = 90
-        self.display = ST7789(display_bus, width=self.dimensions[0], height=self.dimensions[1], rotation=self.rotation)
+        display_bus = FourWire(
+            spi, 
+            command=board.GP20, 
+            chip_select=board.GP17, 
+            reset=board.GP21,
+            baudrate=62500000
+        )
 
-    def testdraw(self):
-        bitmap = displayio.Bitmap(self.dimensions[0], self.dimensions[1], 1)
-        palette = displayio.Palette(1)
-        palette[0] = 0x1cfc0c 
+        self.width = int(320 / 4)
+        self.height = int(240 / 4)
         
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
-        group = displayio.Group()
-        group.append(tile_grid)
+        self.display = ST7789(
+            display_bus, 
+            width=320, 
+            height=240, 
+            rotation=90, 
+            auto_refresh=False
+        )
 
-        # Use self.display to access it
-        self.display.root_group = group
+        self.stage = displayio.Group(scale=2)
+        self.display.root_group = self.stage
+
+        self.max_sprites = 40 
+        self.sprite_pool = []
+        
+        self.shared_bmp = displayio.Bitmap(1, 1, 1)
+        
+        for _ in range(self.max_sprites):
+            pal = displayio.Palette(1)
+            pal[0] = 0x000000
+            
+            tg = displayio.TileGrid(self.shared_bmp, pixel_shader=pal)
+            
+            container = displayio.Group()
+            container.append(tg)
+            container.x = -100 # Hide it
+            
+            self.stage.append(container)
+            self.sprite_pool.append(container)
+        
+        self.pool_index = 0
+
+    def clear(self):
+        """Move used sprites off-screen"""
+        for i in range(self.pool_index):
+            self.sprite_pool[i].x = -100
+        self.pool_index = 0
+
+    def draw_square(self, x, y, size, color):
+        """Uses group scaling to resize a 1x1 pixel into a square"""
+        if self.pool_index < self.max_sprites:
+            container = self.sprite_pool[self.pool_index]
+            tg = container[0]
+            
+            container.x = int(x)
+            container.y = int(y)
+            container.scale = int(size)
+            tg.pixel_shader[0] = color
+            
+            self.pool_index += 1
+
+    def draw_pixel(self, x, y, color):
+        self.draw_square(x, y, 1, color)
+
+    def render(self):
+        self.display.refresh()
